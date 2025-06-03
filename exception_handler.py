6c327898f288e46c605fe8d984b8a6cc1f61388a
nayonsoso <panda0329@naver.com>
2025-06-03 16:36:06 +0900

    refactor: 예외 처리 수정

--- exception_handler.py
+++ exception_handler.py
@@ -1,8 +1,3 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
-
-app = FastAPI()
-
 class BadRequestException(Exception):
     def __init__(self, message: str):
         self.message = message
@@ -10,17 +5,3 @@ class BadRequestException(Exception):
 class InternalServerException(Exception):
     def __init__(self, message: str):
         self.message = message
-
-@app.exception_handler(BadRequestException)
-async def bad_request_exception_handler(request: Request, exc: BadRequestException):
-    return JSONResponse(
-        status_code=400,
-        content={"number": 400, "message": exc.message}
-    )
-
-@app.exception_handler(InternalServerException)
-async def internal_server_exception_handler(request: Request, exc: InternalServerException):
-    return JSONResponse(
-        status_code=500,
-        content={"number": 500, "message": exc.message}
-    )
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import logging
 
 from starlette.middleware.cors import CORSMiddleware
+from exception_handler import BadRequestException, InternalServerException
 from routers.main_router import router as main_router
 from routers.llm_router import router as llm_router
 from routers.chroma_router import router as query_router
 from routers.rag_router import router as rag_router
 from services.chroma_utils import init_chroma
 from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 # ----------------------
 # 1) 로깅 설정
@@ -22,6 +24,22 @@ logger = logging.getLogger(__name__)
 # 2) FastAPI 앱 초기화
 # ----------------------
 app = FastAPI(on_startup=[init_chroma])
+
+
+@app.exception_handler(BadRequestException)
+async def bad_request_exception_handler(request: Request, exc: BadRequestException):
+    return JSONResponse(
+        status_code=400,
+        content={"number": 400, "message": exc.message}
+    )
+
+
+@app.exception_handler(InternalServerException)
+async def internal_server_exception_handler(request: Request, exc: InternalServerException):
+    return JSONResponse(
+        status_code=500,
+        content={"number": 500, "message": exc.message}
+    )
 
 # ----------------------
 # 라우터 등록
